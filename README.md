6547b5b4e0f018dd6d39db15c3117c7555487637
Stephen Braitsch <stephen@quietless.com>
2013-01-30 18:31:57 -0800

    initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+##Node-LED
+
+####A WIP to test communication between an Arduino and a Node.js Server.
+
+***
---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Node-LED
+ * Author : Stephen Braitsch
+ */
+
+var express = require('express')
+  , http = require('http')
+  , SerialPort = require("serialport").SerialPort
+
+var app = express();
+// var port = new SerialPort("/dev/tty-usbserial1", { baudrate: 9600 });
+
+app.configure(function(){
+	app.set('port', 8080);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.locals.pretty = true;
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: 'super-duper-secret-secret' }));
+	app.use(express.methodOverride());
+	app.use(require("stylus").middleware({
+		compress: true,
+		src: __dirname + "/public"
+	}));
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
+
+/* ROUTES */
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
