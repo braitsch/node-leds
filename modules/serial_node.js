e6e5100fc7c0730a3ba7ff6666d8141b06474309
Stephen Braitsch <stephen@quietless.com>
2013-03-07 19:17:47 -0800

    refactoring arduino detection methods

--- modules/serial_node.js
+++ modules/serial_node.js
@@ -8,20 +8,23 @@ exports.writeNumber = function(n)
 // parse number into multiplier and remainder //
 	var m = Math.floor(n/255);
 	var r = n%255;
-	console.log('sending to arduino ::', 1, m, r);
-	sendToArduino(new Buffer([1, m, r]));
+	var data = [1, m, r];
+	console.log('sending to arduino ::', data);
+	sendDataToArduino(new Buffer(data));
 }
 
 exports.setAnimation = function(n)
 {
-	console.log('sending to arduino ::', 2, n);
-	sendToArduino(new Buffer([2, n]));
+	var data = [2, n];
+	console.log('sending to arduino ::', data);
+	sendDataToArduino(new Buffer(data));
 }
 
 exports.setAutoMode = function(n)
 {
-	console.log('sending to arduino ::', 3, n);
-	sendToArduino(new Buffer([3, n]));
+	var data = [3, n];
+	console.log('sending to arduino ::', data);
+	sendDataToArduino(new Buffer(data));
 }
 
 /*
@@ -33,46 +36,46 @@ var socket = require('./socket_server');
 var serialport = require('serialport');
 var exec = require('child_process').exec;
 
-var getArduinoPort = function(callback)
+/*
+	Attempt to connect to Arduino
+*/
+
+var getArduinoPort = function()
 {
+	var port;
 // attempt to detect an arduino connected to mac osx //
+	console.log('* attempting to detect arduino on mac osx *');
 	exec('ls /dev/tty.*', function(error, stdout, stderr){
-		var port;
 		if (stdout){
 			var ports = stdout.split('\n');
 			for (var i = ports.length - 1; i >= 0; i--){
 				if (ports[i].search('usbmodem') != -1 || ports[i].search('usbserial') != -1) port = ports[i];
 			}
 		}
-		callback(port);
 	});
-}
-
-/*
-	Attempt to connect to Arduino
-*/
-
-getArduinoPort(function(port){
 	if (port){
-		attemptConnection(port);
-		return;
+		attemptConnection(port)
 	}	else{
+// attempt to detect an arduino connected to a raspberry pi //
+		console.log('* attempting to detect arduino on raspberry pi *');
 		serialport.list(function (e, ports) {
-		// find the port on the raspberry pi the arduino is connected to //
-			ports.forEach(function(port) {
-				if (port.hasOwnProperty('pnpId')){
+			ports.forEach(function(obj) {
+				if (obj.hasOwnProperty('pnpId')){
 			// FTDI captures the duemilanove //
 			// Arduino captures the leonardo //
-					if (port.pnpId.search('FTDI') != -1 || port.pnpId.search('Arduino') != -1) {
-						attemptConnection(port.comName);
-						return;
+					if (obj.pnpId.search('FTDI') != -1 || obj.pnpId.search('Arduino') != -1) {
+						port = obj.comName;
 					}
 				}
 			});
 		});
 	}
-	console.log('* failed to find arduino : please check your connections *');
-});
+	if (port){
+		attemptConnection(port)
+	}	else{
+		console.log('* failed to find arduino : please check your connections *');
+	}
+}
 
 var attemptConnection = function(port)
 {
@@ -81,13 +84,13 @@ var attemptConnection = function(port)
 	arduino.on("open", function () {
 		console.log('* connection to arduino successful ! *');
 		arduino.on('data', function(data){
-		// send incoming data from arduino out to the socket server //
+	// send incoming data from arduino out to the socket server //
 			socket.onDataFromArduino(data);
 		});
 	});
 }
 
-var sendToArduino = function(buffer)
+var sendDataToArduino = function(buffer)
 {
 // calling write if an arduino is not connected will crash the server! //
 	if (arduino){
@@ -100,3 +103,5 @@ var sendToArduino = function(buffer)
 		});
 	}
 }
+
+getArduinoPort();
